28cd568593b49b5049eab7564f4533bb765ad064
Yashasvi213 <yashasvisharma1369@gmail.com>
2024-04-10 21:11:19 +0530

    update

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,20 +1,41 @@
 //this will be an class component
 import React from "react"
 class AddContact extends React.Component{
+    state = {
+        name:"",
+        email:""
+    }
+
+    add= (e)=>{
+        e.preventDefault();
+        if(this.state.name===""||this.state.email===""){
+            alert("All fields are mandatory");
+            return;
+        }
+        this.setState({name:"",email:""})
+        console.log(this.state);
+        
+    }
+
+    // updateState=(e)=>{
+    //     this.setState(name:e.target.value)
+    // }
+
     render(){
         return(
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
 
                     <div className="field">
                         <label>Name : </label>
-                        <input type="text" name="name" placeholder="name"></input>
+                        <input type="text" name="name" placeholder="name" value={this.state.name} onChange={(e)=>this.setState({name : e.target.value})}></input>
                     </div>
 
                     <div className="field">
                         <label>Email : </label>
-                        <input type="text" name="email" placeholder="email"></input>
+                        <input type="text" name="email" placeholder="email" value={this.state.email} onChange={(e)=>this.setState({email : e.target.value})}></input>
+                        <button className="ui button blue">Add</button>
                     </div>
                 </form>
             </div>
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 function App() {
+  const [contacts, setContacts]=useState([]);
+  //I m taking contact in props 
+  // const AddContactHandler= (contact)=>{
+  //   console.log(contact);
+  // }
+  //AddContactHandler={AddContactHandler}
+
   return (
-    <div className="App">
+    <div className="ui container">
       <Header/>
-      <AddContact/>
-      <ContactList/>
+      <AddContact />
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
