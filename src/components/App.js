02ff4810dd33825b56852d0f46ef537c915e268c
Yashasvi213 <yashasvisharma1369@gmail.com>
2024-04-24 11:26:30 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import { v4 as uuid } from "uuid";
 import './App.css';
+
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
@@ -10,9 +12,18 @@ function App() {
   // I m taking contact in props  contact of ContactCard 
   const AddContactHandler= (contact)=>{
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id:uuid(),...contact}]);
     //...contacts refers to old contact
   }
+
+  const removeContactHandler =(id)=>{
+    const newContactList= contacts.filter((contact)=>{
+      return contact.id !=id;
+    });
+    //if it is true then the particular contact is included otherwise it is not
+    setContacts(newContactList);
+  }
+
   useEffect(() => {
     if (contacts.length > 0) {
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
@@ -36,7 +47,7 @@ function App() {
     <div className="ui container">
       <Header/>
       <AddContact AddContactHandler = {AddContactHandler} />
-      <ContactList contacts={contacts}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
